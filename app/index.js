5614ed4b0a5e52c51bbc0e3aa62f06f973a20949
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-01-17 17:26:40 +0100

    generate dynamically UI controls for each demo

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
+import DEMOS_DESCRIPTIONS from './demos-descriptions.json'
 import './index.css'
+console.log(DEMOS_DESCRIPTIONS)
 ;(async () => {
   if (CSS.paintWorklet === undefined) {
     await import('https://unpkg.com/css-paint-polyfill')
@@ -8,49 +10,66 @@ import './index.css'
 })()
 
 if ('registerProperty' in CSS) {
-  window.CSS.registerProperty({
-    name: '--bezier-curves-number-points',
-    syntax: '<number>',
-    inherits: false,
-    initialValue: 2,
-  })
-  window.CSS.registerProperty({
-    name: '--bezier-curves-stroke-width',
-    syntax: '<number>',
-    inherits: false,
-    initialValue: 3,
-  })
-  window.CSS.registerProperty({
-    name: '--bezier-curves-stroke-color',
-    syntax: '<color>',
-    inherits: false,
-    initialValue: 'orange',
-  })
-  window.CSS.registerProperty({
-    name: '--bezier-curves-helper-radius',
-    syntax: '<number>',
-    inherits: false,
-    initialValue: 5,
-  })
-  window.CSS.registerProperty({
-    name: '--bezier-curves-helper-background-color',
-    syntax: '<color>',
-    inherits: false,
-    initialValue: '#c0392b',
-  })
-  window.CSS.registerProperty({
-    name: '--bezier-curves-helper-accent-color',
-    syntax: '<color>',
-    inherits: false,
-    initialValue: '#f1c40f',
-  })
+  for (let i = 0; i < DEMOS_DESCRIPTIONS.length; i++) {
+    const description = DEMOS_DESCRIPTIONS[i]
+    for (const [key, value] of Object.entries(description.cssVariables)) {
+      CSS.registerProperty({
+        name: key,
+        ...value,
+      })
+    }
+  }
 }
 
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-  const a = document.querySelector('[name="helper-point-background-color"')
-  a.addEventListener('change', (val) => {
-    console.log(a.value)
-  })
+  for (let i = 0; i < DEMOS_DESCRIPTIONS.length; i++) {
+    const description = DEMOS_DESCRIPTIONS[i]
+
+    const $demoEl = document.querySelector(`[data-name="${description.title}"]`)
+    const $demoPreviewEl = $demoEl.getElementsByClassName('demo-preview')[0]
+    const $controlsWrapperEl = $demoEl.getElementsByClassName('demo-control-wrapper')[0]
+
+    for (const [key, value] of Object.entries(description.cssVariables)) {
+      const id = key.substring(2)
+
+      const $wrapperEl = document.createElement('div')
+      $wrapperEl.classList.add('control-wrapper')
+
+      const $label = document.createElement('label')
+      $label.setAttribute('for', key)
+      $label.innerHTML = key
+
+      const $output = document.createElement('div')
+      $output.innerText = value.initialValue
+
+      const $input = document.createElement('input')
+      if (value.syntax === '<number>') {
+        const minValue = value.minValue || 1
+        const maxValue = value.maxValue || 50
+
+        $input.setAttribute('type', 'range')
+        $input.setAttribute('min', minValue)
+        $input.setAttribute('max', maxValue)
+      } else if (value.syntax === '<color>') {
+        $input.setAttribute('type', 'color')
+      }
+      $input.setAttribute('id', id)
+      $input.setAttribute('name', id)
+      $input.setAttribute('value', value.initialValue)
+      $input.addEventListener('change', function () {
+        $demoPreviewEl.style.setProperty(key, this.value)
+        $output.innerText = this.value
+      })
+
+      $wrapperEl.appendChild($label)
+      const $inputWrapper = document.createElement('div')
+      $inputWrapper.classList.add('input-wrapper')
+      $inputWrapper.appendChild($input)
+      $inputWrapper.appendChild($output)
+      $wrapperEl.appendChild($inputWrapper)
+      $controlsWrapperEl.appendChild($wrapperEl)
+    }
+  }
 }
